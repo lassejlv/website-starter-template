8c866516f7ea6fe6e16986253cbe1e85f6233bf1
lassev05 <vestergaardlasse2@gmail.com>
2022-09-30 16:12:22 +0200

    Remove Old Files

--- install.bat
+++
@@ -1,1 +0,0 @@
-npm install -g lite-server
--- src/scripts/index.js
+++
@@ -1,5 +0,0 @@
-console.log("This website is so nice! 😎");
-
-setTimeout(function () {
-  alert("Are still there?\n\nClick 'OK' if you is here 🙂");
-}, 90000);
